1c698d00e8f120ac5fe965b594c7169479e6c396
eprequejo <erequejo@companywatch.net>
2020-05-19 14:21:32 +0100

    Install lib demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MyLibModule } from "my-lib";
+// import { MyLibModule } from "my-lib";
+import { MyLibModule } from "@eprequejo/lib-demo";
 
 @NgModule({
   declarations: [
